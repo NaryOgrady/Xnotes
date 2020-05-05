e6eae0026ada9694fbd7405f07c6808b07497953
NaryOgrady <josel.garza@outlook.com>
2020-05-04 19:46:53 -0500

    Detect screen size and create side panel

--- note.py
+++ note.py
@@ -4,8 +4,11 @@ from curses import wrapper
 
 def curses_main(main_screen):
     main_screen.addstr('test')
+    max_y, max_x = main_screen.getmaxyx()
+    sidebar = curses.newwin(max_y, 78, 0, 0)
+    sidebar.box()
     main_screen.refresh()
-
+    sidebar.refresh()
     curses.napms(2000)
 
 
