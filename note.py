16a8c1c6eec7df0be7a0302d0f46b05248726cb9
NaryOgrady <josel.garza@outlook.com>
2020-05-24 01:51:35 -0500

    Add layout for side and main area

--- note.py
+++ note.py
@@ -3,13 +3,22 @@ from curses import wrapper
 
 
 def curses_main(main_screen):
-    main_screen.addstr('test')
-    max_y, max_x = main_screen.getmaxyx()
-    sidebar = curses.newwin(max_y, 78, 0, 0)
-    sidebar.box()
-    main_screen.refresh()
-    sidebar.refresh()
+    curses.start_color()
+    curses.use_default_colors()
+    setup_layout(main_screen)
     curses.napms(2000)
+
+
+def setup_layout(main_screen):
+    sidebar = curses.newwin(curses.LINES, int(curses.COLS * 0.25), 0, 0)
+    sidebar.box()
+    sidebar.addstr(1, 20, 'XNOTES')
+    main = curses.newwin(curses.LINES, int(
+        curses.COLS * 0.75), 0, int(curses.COLS * 0.25))
+    main.box()
+    main_screen.refresh()
+    main.refresh()
+    sidebar.refresh()
 
 
 if __name__ == '__main__':
